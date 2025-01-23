7b84e22847da5a9d9adfea1b0f650ade4f72f9b4
Vlad Utyumov <utyv108@gmail.com>
2025-01-23 22:58:18 +0500

    Update solo.c

--- keyboards/tightyl46/keymaps/kolobok/solo.c
+++ keyboards/tightyl46/keymaps/kolobok/solo.c
@@ -6,10 +6,9 @@ const uint8_t PROGMEM dict_common[] = {
 	, LO_HI(KC_BSPC), KC_BSPC, NC
 	, LO_HI(CC_SD), KC_ENT, NC
 	, LO_HI(CC_DD), KC_DEL, NC
-	, LO_HI(KC_DEL), KC_MINS, NC
-	, LO_HI(KC_F2), SFN, KC_9, NC // (
-	, LO_HI(KC_F1), SFN, KC_0, NC // )
+	//, LO_HI(CC_W5), KC_MINS, NC // -
 	, LO_HI(KC_SPC), KC_SPC, NC
+	//, LO_HI(KC_F5),       
 	
 	, 0, 0
 };
@@ -20,9 +19,9 @@ const uint8_t PROGMEM dict_ru[] = {
 	, LO_HI(RU_V), RU_MZ, NC
 	, LO_HI(RU_K), RU_Q, NC
 	#ifdef USE_ALTCODE
-	, LO_HI(RU_T),            SFF, AC2(5, 9), NC // ;
+	, LO_HI(RU_T),            SFF, AC2(4, 6), ALF, NC // . 
 	#else
-	, LO_HI(RU_T),            SFN, KC_4, NC // ;
+	, LO_HI(RU_T),            SFF, RU_DOT, NC // . 
 	#endif
 	, LO_HI(KC_CNCL), RU_Z, NC
 	, LO_HI(RU_E), RU_V, NC
@@ -43,13 +42,20 @@ const uint8_t PROGMEM dict_ru[] = {
 	, LO_HI(RU_MZ), RU_R, NC
 	, LO_HI(CC_IMZ), RU_J, NC
 	
-	, LO_HI(KC_1), RU_F, NC
+	, LO_HI(KC_1), KC_MINS, NC
 	, LO_HI(KC_2), RU_EE, NC
-	, LO_HI(KC_4), RU_H, NC
+	#ifdef USE_ALTCODE
+	, LO_HI(KC_4),            SFF, AC2(5, 9), NC // ;
+	#else
+	, LO_HI(KC_4),            SFN, KC_4, NC // ;
+	#endif
 	, LO_HI(KC_8), RU_Y, NC
+	, LO_HI(KC_DEL),          SFF, KC_SPC, KC_EQL, KC_SPC, NC // =
 	, LO_HI(CC_W5), RU_B, NC
 	, LO_HI(KC_F8), RU_M, NC
 	, LO_HI(KC_F4), RU_P, NC
+	, LO_HI(KC_F2), SFN, KC_9, NC // (
+	, LO_HI(KC_F1), SFN, KC_0, NC // )
 	, LO_HI(CC_PJ), RU_WW, NC
 	
 	#ifdef USE_ALTCODE
@@ -57,21 +63,23 @@ const uint8_t PROGMEM dict_ru[] = {
 	#else
 	, LO_HI(KC_F3),            SFN, RU_DOT, KC_SPC, NC // ,
 	#endif
-	#ifdef USE_ALTCODE
-	, LO_HI(KC_F5),       SFF, AC2(4, 6), ALF, NC // .
-	#else
-	, LO_HI(KC_F5),       SFF, RU_DOT, NC // .
-	#endif
 
 	, 0, 0
 };
 
 const uint8_t PROGMEM dict_ru_sft[] = {
 	#ifdef USE_ALTCODE
-	  LO_HI(RU_T),            SFF, AC2(5, 8), NC // :
+	  LO_HI(RU_T),             SFF, AC2(4, 7), NC // /
 	#else
-	  LO_HI(RU_T),            SFN, KC_6, NC // :
+	  LO_HI(RU_T),             SFN, KC_BSLS, NC // /
 	#endif
+	, LO_HI(KC_1), SFN, KC_MINS, NC  // _
+	#ifdef USE_ALTCODE
+	, LO_HI(KC_4),            SFF, AC2(5, 8), NC // :
+	#else
+	, LO_HI(KC_4),            SFN, KC_6, NC // :
+	#endif
+	, LO_HI(KC_DEL), SFN, KC_EQL, NC // +
 	#ifdef USE_ALTCODE
 	, LO_HI(KC_F2),             SFF, AC2(6, 0), NC // <
 	#else
@@ -82,58 +90,64 @@ const uint8_t PROGMEM dict_ru_sft[] = {
 	#else
 	, LO_HI(KC_F1),             CMD, LSW, SFN, KC_DOT, CMD, LSW, NC // >
 	#endif
-	, LO_HI(KC_F3), SFF, KC_EQL, NC
 	#ifdef USE_ALTCODE
-	, LO_HI(KC_F5),       SFF, AC2(3, 4), NC // "
- 	#else
-	, LO_HI(KC_F5),       SFN, KC_2, NC // "
+	, LO_HI(KC_F3),            SFF, AC2(3, 4), NC // " 
+	#else
+	, LO_HI(KC_F3),            SFN, KC_2, NC // " 
 	#endif
 	
 	, 0, 0
 };
 
 const uint8_t PROGMEM dict_ru_sym[] = {
-	  LO_HI(RU_S),            RU_TZ, NC
+	#ifdef USE_ALTCODE
+	  LO_HI(CC_DU),      SFF, AC2(9, 6), NC // `
+ 	#else
+	  LO_HI(CC_DU),      CMD, LSW, SFF, KC_GRV, CMD, LSW, NC // `
+	#endif
+	#ifdef USE_ALTCODE
+	, LO_HI(RU_S),            SFF, AC2(4, 6), ALF, NC // . 
+	#else
+	, LO_HI(RU_S),            SFF, RU_DOT, NC // . 
+	#endif
 	, LO_HI(RU_V),            SFF, KC_7, NC
 	, LO_HI(RU_K),            SFF, KC_8, NC
 	, LO_HI(RU_T),            SFF, KC_9, NC
-	, LO_HI(RU_D),            RU_YO, NC
-	, LO_HI(RU_B),            SFF, KC_4, NC
-	, LO_HI(RU_P),            SFF, KC_5, NC
-	, LO_HI(RU_N),            SFF, KC_6, NC
-	, LO_HI(KC_ENT),          SFF, KC_0, NC
-	, LO_HI(KC_2),            SFF, KC_1, NC
-	, LO_HI(KC_4),            SFF, KC_2, NC
-	, LO_HI(KC_8),            SFF, KC_3, NC
-	, LO_HI(KC_BSPC),  CLN, KC_BSPC, NC
+	, LO_HI(KC_BSPC), 	      CLN, KC_BSPC, NC
+	
+	, LO_HI(CC_SD), 		  RU_YO, NC
+	, LO_HI(RU_D),            SFF, KC_0, NC
+	, LO_HI(RU_B),            SFF, KC_1, NC
+	, LO_HI(RU_P),            SFF, KC_2, NC
+	, LO_HI(RU_N),            SFF, KC_3, NC
+	, LO_HI(KC_ENT),          RU_TZ, NC
+	
+	#ifdef USE_ALTCODE
+	, LO_HI(KC_1),            SFF, AC2(4, 4), NC // ,
+	#else
+	, LO_HI(KC_1),            SFN, RU_DOT, NC // ,
+	#endif
+	, LO_HI(KC_2),            SFF, KC_4, NC
+	, LO_HI(KC_4),            SFF, KC_5, NC
+	, LO_HI(KC_8),            SFF, KC_6, NC
+	, LO_HI(KC_DEL),          SFN, KC_9, KC_0, NC // ()
 
 
-	#ifdef USE_ALTCODE
-	, LO_HI(RU_E),             SFF, AC2(4, 7), NC // /
-	#else
-	, LO_HI(RU_E),             SFN, KC_BSLS, NC // /
-	#endif
-	#ifdef USE_ALTCODE
-	, LO_HI(RU_O),             SFF, AC3(1, 2, 4), NC // |
-	#else
-	, LO_HI(RU_O),             CMD, LSW, SFN, KC_BSLS, CMD, LSW, NC // |
-	#endif
-	, LO_HI(RU_A), SFN, KC_EQL, NC // +
-	#ifdef USE_ALTCODE
-	, LO_HI(RU_I),       SFF, AC2(3, 7), NC // %
- 	#else
-	, LO_HI(RU_I),       SFN, KC_5, NC // %
-	#endif
+	, LO_HI(KC_CNCL), RU_Z, RU_N, NC // зн
+	, LO_HI(RU_E), RU_V, RU_N, NC // вн
+	, LO_HI(RU_O), RU_K, RU_T, NC // кт
+	, LO_HI(RU_A), RU_S, RU_D, NC // сд
+	, LO_HI(RU_I), RU_F, NC
+	
+	, LO_HI(KC_LALT), RU_B, RU_L, NC // бл
 	, LO_HI(RU_Y),            RU_H, NC
 	, LO_HI(RU_U),            RU_YU, NC
 	, LO_HI(RU_Q),            RU_C, NC
 	, LO_HI(RU_MZ),           RU_ZH, NC
-	, LO_HI(KC_F8),           SFN, KC_1, NC // !
-	#ifdef USE_ALTCODE
-	, LO_HI(KC_F4),             SFF, AC2(6, 3), NC // ?
-	#else
-	, LO_HI(KC_F4),             SFN, KC_7, NC // ?
-	#endif
+	
+	, LO_HI(CC_W5), RU_B, RU_N, NC // бн
+	, LO_HI(KC_F8), RU_M, RU_N, NC // мн
+	, LO_HI(KC_F4), RU_V, RU_M, NC // вм
 	#ifdef USE_ALTCODE
 	, LO_HI(KC_F2),             SFF, AC2(9, 1), NC // [
 	#else
@@ -149,42 +163,72 @@ const uint8_t PROGMEM dict_ru_sym[] = {
 	#else
 	, LO_HI(KC_F3),             CMD, LSW, SFN, KC_2, CMD, LSW, NC // @
 	#endif
-	#ifdef USE_ALTCODE
-	, LO_HI(KC_F5),             SFF, AC2(3, 5), NC // #
-	#else
-	, LO_HI(KC_F5),             CMD, LSW, SFN, KC_3, CMD, LSW, NC // #
-	#endif
 	
 	, 0, 0
 };
 
 const uint8_t PROGMEM dict_ru_sym_sft[] = {
 	#ifdef USE_ALTCODE
-	  LO_HI(RU_E),             SFF, AC2(9, 2), NC // \*
+	  LO_HI(CC_DU),       SFF, AC3(1, 2, 6), NC // ~
+ 	#else
+	  LO_HI(CC_DU),       CMD, LSW, SFN, KC_GRV, CMD, LSW, NC // ~
+	#endif
+	#ifdef USE_ALTCODE
+	, LO_HI(RU_S),             SFF, AC2(9, 2), NC // \*
 	#else
-	  LO_HI(RU_E),             SFF, KC_BSLS, NC // \*
+	, LO_HI(RU_S),             SFF, KC_BSLS, NC // \*
 	#endif
 	#ifdef USE_ALTCODE
-	, LO_HI(RU_O),             SFF, AC2(3, 8), NC // &
+	, LO_HI(RU_V),             SFF, AC2(3, 8), NC // &
 	#else
-	, LO_HI(RU_O),             CMD, LSW, SFN, KC_7, CMD, LSW, NC // &
+	, LO_HI(RU_V),             CMD, LSW, SFN, KC_7, CMD, LSW, NC // &
 	#endif
-	, LO_HI(RU_A), SFN, KC_8, NC // *
+	, LO_HI(RU_K), SFN, KC_8, NC // *
 	#ifdef USE_ALTCODE
-	, LO_HI(RU_I),       SFF, AC3(1, 2, 6), NC // ~
- 	#else
-	, LO_HI(RU_I),       CMD, LSW, SFN, KC_GRV, CMD, LSW, NC // ~
+	, LO_HI(RU_T),             SFF, AC3(1, 2, 4), NC // |
+	#else
+	, LO_HI(RU_T),             CMD, LSW, SFN, KC_BSLS, CMD, LSW, NC // |
+	#endif
+	
+	#ifdef USE_ALTCODE
+	, LO_HI(RU_D),             SFF, AC2(3, 9), NC // '
+	#else
+	, LO_HI(RU_D),             CMD, LSW, KC_QUOT, CMD, LSW, NC // '
+	#endif
+	, LO_HI(RU_B),             SFN, KC_1, NC // !
+	#ifdef USE_ALTCODE
+	, LO_HI(RU_P),             SFF, AC2(6, 3), NC // ?
+	#else
+	, LO_HI(RU_P),             SFN, KC_7, NC // ?
 	#endif
 	#ifdef USE_ALTCODE
-	, LO_HI(KC_F8),      SFF, AC3(2, 5, 2), NC // №
- 	#else
-	, LO_HI(KC_F8),      CMD, LSW, SFN, KC_3, CMD, LSW, NC // №
+	, LO_HI(RU_N),             SFF, AC2(3, 5), NC // #
+	#else
+	, LO_HI(RU_N),             CMD, LSW, SFN, KC_3, CMD, LSW, NC // #
+	#endif
+	
+	, LO_HI(KC_1),          SFF, KC_EQL, NC // =
+	#ifdef USE_ALTCODE
+	, LO_HI(KC_2),             SFF, AC2(3, 6), NC // $
+	#else
+	, LO_HI(KC_2),             CMD, LSW, SFN, KC_4, CMD, LSW, NC // $
 	#endif
 	#ifdef USE_ALTCODE
-	, LO_HI(KC_F4),      SFF, AC2(9, 6), NC // `
+	, LO_HI(KC_4),       SFF, AC2(3, 7), NC // %
  	#else
-	, LO_HI(KC_F4),      CMD, LSW, SFF, KC_GRV, CMD, LSW, NC // `
+	, LO_HI(KC_4),       SFN, KC_5, NC // %
 	#endif
+	#ifdef USE_ALTCODE
+	, LO_HI(KC_8),      SFF, AC3(2, 5, 2), NC // №
+ 	#else
+	, LO_HI(KC_8),      SFN, KC_3, NC // №
+	#endif
+	#ifdef USE_ALTCODE
+	, LO_HI(KC_DEL),       SFF, AC2(6, 0), ALF, AC2(6, 2), NC // <>
+	#else
+	, LO_HI(KC_DEL),       CMD, LSW, SFN, KC_COMM, KC_DOT, CMD, LSW, NC // <>
+	#endif
+
 	#ifdef USE_ALTCODE
 	, LO_HI(KC_F2),             SFF, AC3(1, 2, 3), NC // {
 	#else
@@ -199,11 +243,6 @@ const uint8_t PROGMEM dict_ru_sym_sft[] = {
 	, LO_HI(KC_F3),             SFF, AC2(9, 4), NC // ^
 	#else
 	, LO_HI(KC_F3),             CMD, LSW, SFN, KC_6, CMD, LSW, NC // ^
-	#endif
-	#ifdef USE_ALTCODE
-	, LO_HI(KC_F5),             SFF, AC2(3, 9), NC // '
-	#else
-	, LO_HI(KC_F5),             CMD, LSW, KC_QUOT, CMD, LSW, NC // '
 	#endif
 	
 	, 0, 0
